b02e6925474d8f8691bee77c4f59d84720b946c9
quanptatb <quanptatb01758@gmail.com>
2026-01-15 09:11:38 +0700

    Lab04 99%

---
+++ Lab04/Lab04/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Hello from "./components/Hello.vue";
+  import Bai03 from "./components/Bai03.vue";
+  import Bai04 from "./components/Bai04.vue";
+</script>
+
+<template>
+  <!-- <Hello /> -->
+  <!-- <Bai03 /> -->
+  <Bai04 />
+</template>
+
+<style scoped></style>
