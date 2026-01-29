568c2555ac377abb053e226f24ad192dd90fa940
quanptatb <quanptatb01758@gmail.com>
2026-01-29 09:21:40 +0700

    done lab5

---
+++ Lab05/Bai01/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import btnupdate from './components/btnchanging.vue';
+import btninput from './components/btninput.vue';
+import forminput from './components/form.vue';
+</script>
+
+<template>
+  <main>
+    <btnupdate></btnupdate>>
+    <btninput></btninput>
+    <forminput></forminput>
+  </main>
+</template>
